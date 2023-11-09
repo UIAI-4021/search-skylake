8e445d01372ee92058e14f3518588d7845b31a56
Milad Tavakoli <milad.tavakoli81@gmail.com>
2023-11-09 19:41:43 +0330

    first implementation dijkstra function

--- best flight/main.py
+++ best flight/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from queue import PriorityQueue
 
 df = pd.read_csv('Flight_Data.csv')
 input = 'Imam Khomeini International Airport - Raleigh Durham International Airport'.split(' - ')
@@ -7,4 +8,18 @@ destination = input[1]
 
 locations = df.loc[df['SourceAirport'] == source]
 
-print(locations)
+print(locations.iat[2,13])
+
+
+def dijkstra(source, destination):
+    frontier = PriorityQueue()
+    for index in locations.index:
+        frontier.put(df.loc[index])
+    while not frontier.empty:
+        node = frontier.get
+        # check is goal or not
+        children = df.loc[df['SourceAirport'] == source]
+        for index in children.index:
+            frontier.put(df.loc[index])
+        
+    return 'path not found'
