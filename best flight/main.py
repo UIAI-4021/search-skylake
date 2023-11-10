9c8f1e3e49258990f257247521b94d9a048d6012
zahra kazemi <zahra.k199002@gmail.com>
2023-11-10 21:43:53 +0330

    compelete a* algorithm

--- best flight/main.py
+++ best flight/main.py
@@ -49,20 +49,22 @@ def a_star(source , destination):
     locations = df.loc[df['SourceAirport'] == source]
     for row in locations.iterrows():
         reached.append(row[1]['DestinationAirport'])
-
-        frontier.put((row[1]['Distance'], row[0]))
+        f = heuristic(row[1]['SourceAirport'] , destination) + row[1]['Distance']
+        frontier.put((f, row[0]))
         path.update({row[0]: [row[0]]})
     while not frontier.empty():
         pop = frontier.get()
         distance = pop[0]
         node = pop[1]
         if df.loc[node, 'DestinationAirport'] == destination:
+            
             return path[node]
         reached.append(df.loc[node, 'DestinationAirport'])
         children = df.loc[df['SourceAirport'] == df.loc[node, 'DestinationAirport']]
         for children_row in children.iterrows():
             if children_row[1]['DestinationAirport'] not in reached:
-                frontier.put((children_row[1]['Distance'] + distance, children_row[0]))
+                f = heuristic(children_row[1]['SourceAirport'] , destination) + distance
+                frontier.put((f, children_row[0]))
                 path.update({children_row[0]: path[node].copy()+ [children_row[0]]})
 
 
@@ -72,10 +74,14 @@ input = 'Imam Khomeini International Airport - Raleigh Durham International Airp
 #input = 'General Edward Lawrence Logan International Airport - John F Kennedy International Airport'.split(' - ')
 source = input[0]
 destination = input[1]
-finded_path = dijkstra(source,destination)
+finded_path = a_star(source,destination)
 print(finded_path)
-for index in finded_path:
-    print(df.loc[index])
 
 
-heuristic('Dubai International Airport' ,' ')
+distance_sum=0
+
+for i in finded_path:
+
+    distance_sum += df.loc[i, 'Distance']
+
+print('\nTotal Distance: : ' + str(distance_sum) + ' km')
