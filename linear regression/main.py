6ac05b635d814678be8867087ed171c90c800d84
Milad Tavakoli <milad.tavakoli81@gmail.com>
2023-11-12 12:16:35 +0330

    add mae errorr

--- linear regression/main.py
+++ linear regression/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
 
 train = pd.read_csv('Flight_Price_Dataset_Q2.csv')
@@ -38,15 +39,19 @@ stops_map = {
 train['stops'] = train['stops'].map(stops_map)
 train.info()
 
-X_train, X_test, y_train, y_test = train_test_split(train[['departure_time', 'stops', 'arrival_time', 'class', 'duration', 'days_left']], train['price'], random_state=42, shuffle=True) # x , y , percent of test size, random state, shuffle
+X_train, X_test, y_train, y_test = train_test_split(train[['departure_time', 'stops', 'arrival_time', 'class', 'duration', 'days_left']], train['price'], test_size=0.999, random_state=42, shuffle=True) # x , y , percent of test size, random state, shuffle
 
-
+print(X_train)
 w = []
 for i in range(6):
     w.append(np.random.randn(1))
 b = np.random.randn(1)
 
-yp = []
+yp = pd.DataFrame(np.random.randn(1, 1), columns=['price'])
 
 for i in X_train.iterrows():
-    yp[i] = w[0] * X_train[1]['departure_time'] + w[1] * X_train[1]['stops'] + w[2] * X_train[1]['arrival_time'] + w[3] * X_train[1]['class'] + w[4] * X_train[1]['duration'] + w[5] * X_train[1]['days_left'] + b
+    yp.loc[i[0]] = yp.loc[0]
+    yp.loc[i[0]]['price'] = w[0] * i[1]['departure_time'] + w[1] * i[1]['stops'] + w[2] * i[1]['arrival_time'] + w[3] * i[1]['class'] + w[4] * i[1]['duration'] + w[5] * i[1]['days_left'] + b
+    print(yp.loc[i[0]])
+MAE_error = mean_absolute_error(y_train, yp)
+print(MAE_error)
